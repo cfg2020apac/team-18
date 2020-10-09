97a9f6caa7e4e4badefddd1ddb6af9a704387d42
Alvin Harjanto <alvin.chip99@yahoo.com>
2020-10-10 06:16:10 +0800

    Init integration with firebase

---
+++ frontend/src/firebase.js
@@ -0,0 +1,17 @@
+import * as firebase from "firebase";
+import "firebase/firestore";
+
+let config = {
+  apiKey: "AIzaSyBF0frutFE4dtoyFLhB6cz-cfNEOHVDcqg",
+  authDomain: "handson-e01fd.firebaseapp.com",
+  databaseURL: "https://handson-e01fd.firebaseio.com",
+  projectId: "handson-e01fd",
+  storageBucket: "handson-e01fd.appspot.com",
+  messagingSenderId: "160260095861",
+  appId: "1:160260095861:web:1ead83148279c974f99d3c",
+  measurementId: "G-TV92GLKN3R"
+};
+
+firebase.initializeApp(config);
+
+export default firebase.firestore();
