e5440f29cfa3474dfbd7212bb2abd1274624bc88
Alvin Harjanto <alvin.chip99@yahoo.com>
2020-10-10 06:16:10 +0800

    Init integration with firebase

--- frontend/src/pages/EventPage.js
+++ frontend/src/pages/EventPage.js
@@ -1,6 +1,6 @@
-import React, { useState } from "react";
+import React, { useEffect, useState } from "react";
 import { DataGrid } from "@material-ui/data-grid";
-import { Box, Button, Card, Chip, Container, Divider, Grid } from "@material-ui/core";
+import { Box, Button, Card, Chip, CircularProgress, Container, Divider, Grid } from "@material-ui/core";
 import {
   createStyles,
   makeStyles,
@@ -11,6 +11,15 @@ import FaceIcon from '@material-ui/icons/Face';
 import DoneOutlineIcon from '@material-ui/icons/DoneOutline';
 import QueryBuilderIcon from '@material-ui/icons/QueryBuilder';
 import moment from "moment";
+import {
+  BrowserRouter as Router,
+  Switch,
+  Route,
+  useParams
+} from "react-router-dom";
+import EventsDataService from "../services/events.services";
+
+const modelDesc = "At vero eos et accusamus et iusto odio dignissimos ducimus qui blanditiis praesentium voluptatum deleniti atque corrupti quos dolores et quas molestias excepturi sint occaecati cupiditate non provident, similique sunt in culpa qui officia deserunt mollitia animi, id est laborum et dolorum fuga. Et harum quidem rerum facilis est et expedita distinctio. Nam libero tempore, cum soluta nobis est eligendi optio cumque nihil impedit quo minus id quod maxime placeat facere possimus, omnis voluptas assumenda est, omnis dolor repellendus. Temporibus autem quibusdam et aut officiis debitis aut rerum necessitatibus saepe eveniet ut et voluptates repudiandae sint et molestiae non recusandae. Itaque earum rerum hic tenetur a sapiente delectus, ut aut reiciendis voluptatibus maiores alias consequatur aut perferendis doloribus asperiores repellat."
 
 const columns = [
   { field: 'id', headerName: 'ID', width: 70 },
@@ -45,22 +54,22 @@ const rows = [
   { id: 9, lastName: 'Roxie', firstName: 'Harvey', age: 65 },
 ];
 
-const event = {
-  name: "Code For Good 2020",
-  description: "At vero eos et accusamus et iusto odio dignissimos ducimus qui blanditiis praesentium voluptatum deleniti atque corrupti quos dolores et quas molestias excepturi sint occaecati cupiditate non provident, similique sunt in culpa qui officia deserunt mollitia animi, id est laborum et dolorum fuga. Et harum quidem rerum facilis est et expedita distinctio. Nam libero tempore, cum soluta nobis est eligendi optio cumque nihil impedit quo minus id quod maxime placeat facere possimus, omnis voluptas assumenda est, omnis dolor repellendus. Temporibus autem quibusdam et aut officiis debitis aut rerum necessitatibus saepe eveniet ut et voluptates repudiandae sint et molestiae non recusandae. Itaque earum rerum hic tenetur a sapiente delectus, ut aut reiciendis voluptatibus maiores alias consequatur aut perferendis doloribus asperiores repellat.",
-  tags: [
-    "epic",
-    "animals",
-    "exciting",
-    "epic",
-    "animals",
-    "exciting",
-  ],
-  coordinator: "J.P. Morgan",
-  coordinatorEmail: "jp@morgan.com",
-  startDate: "2020-01-17",
-  endDate: "2020-01-18",
-}
+// const event = {
+//   name: "Code For Good 2020",
+//   description: "At vero eos et accusamus et iusto odio dignissimos ducimus qui blanditiis praesentium voluptatum deleniti atque corrupti quos dolores et quas molestias excepturi sint occaecati cupiditate non provident, similique sunt in culpa qui officia deserunt mollitia animi, id est laborum et dolorum fuga. Et harum quidem rerum facilis est et expedita distinctio. Nam libero tempore, cum soluta nobis est eligendi optio cumque nihil impedit quo minus id quod maxime placeat facere possimus, omnis voluptas assumenda est, omnis dolor repellendus. Temporibus autem quibusdam et aut officiis debitis aut rerum necessitatibus saepe eveniet ut et voluptates repudiandae sint et molestiae non recusandae. Itaque earum rerum hic tenetur a sapiente delectus, ut aut reiciendis voluptatibus maiores alias consequatur aut perferendis doloribus asperiores repellat.",
+//   tags: [
+//     "epic",
+//     "animals",
+//     "exciting",
+//     "epic",
+//     "animals",
+//     "exciting",
+//   ],
+//   coordinator: "J.P. Morgan",
+//   coordinatorEmail: "jp@morgan.com",
+//   startDate: "2020-01-17",
+//   endDate: "2020-01-18",
+// }
 
 const useStyles = makeStyles((theme) =>
   createStyles({
@@ -130,8 +139,11 @@ const tagColors = ["#ff1148", "#5b2b95", "#ffc300", "#00bdba"];
 
 export const EventPage = () => {
   const classes = useStyles();
+  const [event, setEvent] = useState(null);
   const [data, setData] = useState([]);
   const [selection, setSelection] = useState([]);
+  const params = useParams();
+  const eventId = params.eventId;
 
   const handleGenerateVolunteers = () => {
     setData(rows);
@@ -152,139 +164,172 @@ export const EventPage = () => {
   }
 
   let future = true;
-  if (moment().isAfter(moment(event.endDate).format("YYYY-MM-DD"))) {
+  if (event && moment().isAfter(event.endDate)) {
     future = false;
   }
 
+  useEffect(() => {
+    EventsDataService
+      .getAll()
+      .doc(eventId)
+      .get()
+      .then(querySnapshot => {
+        if(!querySnapshot.empty) {
+          const event = querySnapshot.data()
+          setEvent({
+            startDate: moment.unix(event.eventStartDate.seconds).format("YYYY-MM-DD"),
+            endDate: moment.unix(event.eventEndDate.seconds).format("YYYY-MM-DD"),
+            name: event.eventName,
+            coordinator: event.coordinator,
+            coordinatorEmail: event.coordinatorEmail,
+            tags: [
+              "epic",
+              "animals",
+              "exciting",
+              "epic",
+              "animals",
+              "exciting",
+            ],
+            description: modelDesc,
+          })
+        }
+      })
+  }, [])
+
   return (
-    <div>
-      <Container className={classes.title}>
-        {event.name}
-      </Container>
-      <Box display="flex">
-        {future ? <Chip
-          className={classes.statusChipUncompleted}
-          icon={<QueryBuilderIcon />}
-          label="Pending"
-          color="primary"
-        /> :
-        <Chip
-          className={classes.statusChipCompleted}
-          icon={<DoneOutlineIcon />}
-          label="Completed"
-          color="primary"
-        />}
-      </Box>
-      <Card className={classes.descCard}>
-        <Grid container className={classes.fields}>
-          <Grid item xs={12} className={classes.fieldTitle}>
-            <b>Description</b>
-          </Grid>
-          <Grid item xs={12}>
-            {event.description}
-          </Grid>
-        </Grid>
-      </Card>
-      <Grid container className={classes.cardWrapper}>
-        <Grid item xs={6}>
-          <Card className={classes.card}>
-            <Grid container className={classes.fields}>
-              <Grid item xs={12} className={classes.tagTitle}>
-                <b>Information</b>
-              </Grid>
-              <Grid item xs={6}>
-                Coordinator
-              </Grid>
-              <Grid item xs={6}>
-                {event.coordinator}
-              </Grid>
-              <Divider />
-              <Grid item xs={6}>
-                Coordinator email
-              </Grid>
-              <Grid item xs={6}>
-                {event.coordinatorEmail}
-              </Grid>
-              <Divider />
-              <Grid item xs={6}>
-                Start Date
-              </Grid>
-              <Grid item xs={6}>
-                {moment(event.startDate).format("YYYY-MM-DD")}
-              </Grid>
-              <Divider />
-              <Grid item xs={6}>
-                End Date
-              </Grid>
-              <Grid item xs={6}>
-                {event.endDate}
-              </Grid>
+    <>
+    {event ? 
+      <div>
+        <Container className={classes.title}>
+          {event.name}
+        </Container>
+        <Box display="flex">
+          {future ? <Chip
+            className={classes.statusChipUncompleted}
+            icon={<QueryBuilderIcon />}
+            label="Pending"
+            color="primary"
+          /> :
+          <Chip
+            className={classes.statusChipCompleted}
+            icon={<DoneOutlineIcon />}
+            label="Completed"
+            color="primary"
+          />}
+        </Box>
+        <Card className={classes.descCard}>
+          <Grid container className={classes.fields}>
+            <Grid item xs={12} className={classes.fieldTitle}>
+              <b>Description</b>
             </Grid>
-          </Card>
-        </Grid>
-        <Grid item xs={5}>
-          <Card className={classes.card}>
-            <Grid container className={classes.fields}>
-              <Grid item xs={12} className={classes.tagTitle}>
-                <b>Tags</b>
+            <Grid item xs={12}>
+              {event.description}
+            </Grid>
+          </Grid>
+        </Card>
+        <Grid container className={classes.cardWrapper}>
+          <Grid item xs={6}>
+            <Card className={classes.card}>
+              <Grid container className={classes.fields}>
+                <Grid item xs={12} className={classes.tagTitle}>
+                  <b>Information</b>
+                </Grid>
+                <Grid item xs={6}>
+                  Coordinator
+                </Grid>
+                <Grid item xs={6}>
+                  {event.coordinator}
+                </Grid>
+                <Divider />
+                <Grid item xs={6}>
+                  Coordinator email
+                </Grid>
+                <Grid item xs={6}>
+                  {event.coordinatorEmail}
+                </Grid>
+                <Divider />
+                <Grid item xs={6}>
+                  Start Date
+                </Grid>
+                <Grid item xs={6}>
+                  {moment(event.startDate).format("YYYY-MM-DD")}
+                </Grid>
+                <Divider />
+                <Grid item xs={6}>
+                  End Date
+                </Grid>
+                <Grid item xs={6}>
+                  {moment(event.endDate).format("YYYY-MM-DD")}
+                </Grid>
               </Grid>
-              <Grid item xs={12}>
-              {event.tags.map((tag, i) => (
-                <Chip
-                  className={classes.chip}
-                  icon={<FaceIcon />}
-                  label={tag}
-                  color="primary"
-                  style={{ color: "white", backgroundColor: tagColors[i%4] }}
+            </Card>
+          </Grid>
+          <Grid item xs={5}>
+            <Card className={classes.card}>
+              <Grid container className={classes.fields}>
+                <Grid item xs={12} className={classes.tagTitle}>
+                  <b>Tags</b>
+                </Grid>
+                <Grid item xs={12}>
+                {event.tags.map((tag, i) => (
+                  <Chip
+                    className={classes.chip}
+                    icon={<FaceIcon />}
+                    label={tag}
+                    color="primary"
+                    style={{ color: "white", backgroundColor: tagColors[i%4] }}
+                  />
+                ))}
+                </Grid>
+              </Grid>
+            </Card>
+          </Grid>
+        </Grid>
+        {future ? <Button
+          variant="contained"
+          color="primary"
+          className={classes.button}
+          onClick={handleGenerateVolunteers}
+        >
+          Generate Potential Volunteers
+        </Button> :
+        <Button
+          variant="contained"
+          color="primary"
+          className={classes.button}
+          onClick={handleGenerateEventReport}
+        >
+          Generate Event Report
+        </Button>}
+        {data.length !== 0 &&
+          <>
+            <Container className={classes.mailingListTitle}>
+              Volunteer Mailing List
+            </Container>
+            <Card className={classes.card}>
+              <div style={{ height: Math.min(data.length*65, 650), width: '100%' }}>
+                <DataGrid
+                  rows={data}
+                  columns={columns}
+                  pageSize={10}
+                  onSelectionChange={handleSelectionChange}
+                  checkboxSelection
                 />
-              ))}
-              </Grid>
-            </Grid>
-          </Card>
-        </Grid>
-      </Grid>
-      {future ? <Button
-        variant="contained"
-        color="primary"
-        className={classes.button}
-        onClick={handleGenerateVolunteers}
-      >
-        Generate Potential Volunteers
-      </Button> :
-      <Button
-        variant="contained"
-        color="primary"
-        className={classes.button}
-        onClick={handleGenerateEventReport}
-      >
-        Generate Event Report
-      </Button>}
-      {data.length !== 0 &&
-        <>
-          <Container className={classes.mailingListTitle}>
-            Volunteer Mailing List
-          </Container>
-          <Card className={classes.card}>
-            <div style={{ height: Math.min(data.length*65, 650), width: '100%' }}>
-              <DataGrid
-                rows={data}
-                columns={columns}
-                pageSize={10}
-                onSelectionChange={handleSelectionChange}
-                checkboxSelection
-              />
-            </div>
-          </Card>
-          <Button
-            variant="contained"
-            color="secondary"
-            className={classes.button}
-            onClick={handleSendEmails}
-          >
-            Send Emails
-          </Button>
-        </>
-      }
-    </div>
+              </div>
+            </Card>
+            <Button
+              variant="contained"
+              color="secondary"
+              className={classes.button}
+              onClick={handleSendEmails}
+            >
+              Send Emails
+            </Button>
+          </>
+        }
+      </div>
+    :
+    <CircularProgress />}
+    </>
   )
 }
--- frontend/src/pages/EventsPage.js
+++ frontend/src/pages/EventsPage.js
@@ -1,4 +1,4 @@
-import * as React from "react";
+import React, { useEffect, useState } from "react";
 import {
   Calendar,
   DateLocalizer,
@@ -8,8 +8,7 @@ import {
   Views,
   Navigate,
   components,
-} from 'react-big-calendar'
-import moment from "moment";
+} from 'react-big-calendar';
 import style from "react-big-calendar/lib/css/react-big-calendar.css";
 
 import { Card, Container } from "@material-ui/core";
@@ -21,6 +20,8 @@ import {
 } from "@material-ui/core/styles";
 
 import { useHistory } from "react-router-dom";
+import EventsDataService from "../services/events.services";
+import moment from "moment";
 
 const events = [
   {
@@ -62,10 +63,33 @@ const localizer = momentLocalizer(moment)
 export const EventsPage = () => {
   const classes = useStyles();
   const history = useHistory();
+  const [events, setEvents] = useState([]);
 
   const handleSelectEvent = (event, e) => {
     history.push(`/events/${event.id}`)
   }
+
+  const onDataChange = (items) => {
+    let events = []
+    items.forEach((item) => {
+      const id = item.id
+      const data = item.data()
+      events.push({
+        id: id,
+        start: moment.unix(data.eventStartDate.seconds).format("YYYY-MM-DD"),
+        end: moment.unix(data.eventEndDate.seconds).format("YYYY-MM-DD"),
+        title: data.eventName,
+        coordinator: data.coordinator,
+        coordinatorEmail: data.coordinatorEmail,
+        allDay: true,
+      })
+    })
+    setEvents(events);
+  }
+
+  useEffect(() => {
+    EventsDataService.getAll().orderBy("eventName", "asc").onSnapshot(onDataChange)
+  }, [])
 
   return (
     <div>
