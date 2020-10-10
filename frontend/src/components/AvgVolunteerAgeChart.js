e44aa71e23c7a49d1e99af4c09cb5885a9b621ea
Mahimaanand <47138963+Mahimaanand@users.noreply.github.com>
2020-10-10 10:09:41 +0800

    final changes to UI

---
+++ frontend/src/components/AvgVolunteerAgeChart.js
@@ -0,0 +1,31 @@
+
+import React from 'react';
+import SelectBox from 'devextreme-react/select-box';
+import {
+  Chart,
+  Series,
+  ArgumentAxis,
+  CommonSeriesSettings,
+  Export,
+  Legend,
+  Margin,
+  Title,
+  Subtitle,
+  Tooltip,
+  Grid
+} from 'devextreme-react/chart';
+
+const types = ['line'];
+
+avgAge = [
+    {year: '2014', age: 18.3},
+    {year: '2015', age: 22.4},
+    {year: '2016', age: 17},
+    {year: '2017', age: 28.8},
+    {year: '2018', age: 23.2},
+    {year: '2019', age: 21.9},
+    {year: '2020', age: 16.4},
+]
+
+
+
